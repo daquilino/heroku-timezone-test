d4799e1229c04e7d93b5549390d3e3052d9e691c
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-05-10 11:27:14 -0400

    server framework

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-<!-- Douglas Aquilino       May 10, 2017 -->
-<!-- Heroku server date() test    -->
-<!--             index.html                -->
+// Douglas Aquilino       May 10, 2017
+// basic express server to test time.
 
+
+// Dependencies
+// ===========================================================
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+
+
+// Routes
+// ===========================================================
+app.get("/", function(req, res) {
+  	
+	var time = "some time";
+ 	res.send("Current Server Time\n", );
+});
+
+
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
